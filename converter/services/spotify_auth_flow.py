264baa0421d630b3535f5f1b42f7c62d914fbd95
Chengye04 <chengye@franknetwork.co>
2025-04-30 17:54:34 +0800

    added function to get all songs

--- converter/services/spotify_auth_flow.py
+++ converter/services/spotify_auth_flow.py
@@ -3,11 +3,96 @@ import os
 from dotenv import load_dotenv
 import base64
 from urllib.parse import urlencode
+from typing import Dict, List
 load_dotenv()
 
 id = os.getenv("SPOTIFY_CLIENT_ID")
 secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
+
+
+#reusable helper functions
+
+def get_private_playlist_ids(access_token):
+#returns array of all playlists
+
+    url = 'https://api.spotify.com/v1/me/playlists'
+
+    headers = {
+        'Authorization': f"Bearer {access_token}"
+    }
+
+    r = requests.get(url, headers=headers) 
+    data = r.json()
+
+    playlists = {}
+
+    for item in data['items']:
+        name = item['name']
+        id = item['id']
+        playlists[name] = {
+            'id': id,
+            'tracks': []
+            }
+
+    return playlists
+
+
+def get_all_songs():
+    #returns nested dictionary of songs.
+    #{'playlist': {'id': 123, 'tracks': ['abc','xyz']}}
+
+    access_token = get_access_token()
+    headers = {
+        'Authorization': f"Bearer {access_token}"
+    }
+
+    playlists = get_private_playlist_ids(access_token)
+
+    for name, data in playlists.items():
+        id = data['id']
+        url = f'https://api.spotify.com/v1/playlists/{id}/tracks'
+
+        r = requests.get(url, headers=headers)
+        r = r.json()
+        
+        for item in r['items']:
+            song_name = item['track']['name']
+            playlists[name]['tracks'].append(song_name)
+    print(playlists)
+    return playlists
+
+
+#reusable function exchanging refresh token for access token
+def get_access_token():
+    token = os.getenv('SPOTIFY_REFRESH_TOKEN')
+
+    url = "https://accounts.spotify.com/api/token"
+
+    encoded = f"{id}:{secret}"
+    encoded = base64.b64encode(encoded.encode()).decode()
+
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': token
+    }
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f"Basic {encoded}"
+    }
+
+    r = requests.post(url, data=data, headers=headers)
+    data = r.json()
+    return data['access_token']
+
+if __name__ == "__main__":
+   #obtain_auth_code()
+   #exchange_code_for_token()
+   
+   get_all_songs()
+
+#one-time use functions to obtain refresh token in spotify authorization code flow
 
 def obtain_auth_code():
     #one-time use function to obtain code from user authentication
@@ -23,8 +108,6 @@ def obtain_auth_code():
     
     login_url += urlencode(params)
     print(login_url)
-
-
 
 def exchange_code_for_token():
     id = os.getenv("SPOTIFY_CLIENT_ID")
@@ -51,53 +134,3 @@ def exchange_code_for_token():
     print(r.text)
 
 
-def get_access_token():
-    token = os.getenv('SPOTIFY_REFRESH_TOKEN')
-
-    url = "https://accounts.spotify.com/api/token"
-
-    encoded = f"{id}:{secret}"
-    encoded = base64.b64encode(encoded.encode()).decode()
-
-    data = {
-        'grant_type': 'refresh_token',
-        'refresh_token': token
-    }
-
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': f"Basic {encoded}"
-    }
-
-    r = requests.post(url, data=data, headers=headers)
-    data = r.json()
-    return data['access_token']
-
-
-def get_private_playlist_names():
-#returns array of all playlists
-
-    access_token = get_access_token()
-    url = 'https://api.spotify.com/v1/me/playlists'
-
-    headers = {
-        'Authorization': f"Bearer {access_token}"
-    }
-
-    r = requests.get(url, headers=headers) 
-    data = r.json()
-
-    playlists = []
-
-    for item in data['items']:
-        playlists.append(item['name'])
-
-    return playlists
-
-
-
-if __name__ == "__main__":
-   #obtain_auth_code()
-   #exchange_code_for_token()
-   #get_access_token()
-   get_private_playlist_names()
--- converter/views.py
+++ converter/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+#.\.venv\Scripts\Activate.ps1
+
 class HelloWorld(APIView):
     def get(self, request):
         return Response({"message": "Hello, DRF is working!"})
